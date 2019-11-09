3883817a52c4d1f87418bc0d236aa13f5d7b9436
catherine244 <ccayreen24@gmail.com>
2019-11-09 15:41:59 +0300

    added a js for contact html

--- js/script.js
+++ js/script.js
@@ -141,3 +141,13 @@ $(document).ready(function(){
   
   
 });
+$("button").click(function(event) {
+  event.preventDefault();
+  var user = document.getElementById('username').value;
+  alert("Dear " + user + ", we have received your message. Thank you for reaching out to us.");
+});
+$("button").on('click', function(){
+  $('form').each(function(){
+    this.reset();
+  });
+});
